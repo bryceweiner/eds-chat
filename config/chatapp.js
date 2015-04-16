f867968cbef8dda09b022be0289cfe7d07aac59f
Rakesh <rakesh@moneypot.com>
2015-04-16 15:42:48 -0500

    Allow to set your own API_URL for development propouses

--- config/chatapp.js
+++ config/chatapp.js
@@ -1,6 +1,6 @@
 'use strict';
 
 export const
-  API_URL    = "https://api.moneypot.com/v1",
+  API_URL    = process.env.API_URL || "https://api.moneypot.com/v1",
   APP_ID     = process.env.APP_ID,
   APP_SECRET = process.env.APP_SECRET;
